8a8014ad89db10d1c3aedf2d499d59c843fc1224
nothoru <yuiishi022@gmail.com>
2025-07-30 23:25:03 +0800

    debug connection

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,6 +4,8 @@ import axios from "axios";
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
+console.log("Axios is configured to use API Base URL:", API_BASE_URL);
+
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: {
